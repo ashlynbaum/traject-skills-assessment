f0da7814420a483083ecd00430470553ba5d9724
Ashlyn Baum <a@ashlynbaum.com>
2015-05-13 15:29:10 +1200

    Added JQuery Gem set navigation and general layout
    added a navigation bar with scrollspy to sections of home page.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
 //= require_tree .
+//= require jquery
+//= require bootstrap-sprockets
+
+// carousel
+$('.carousel').carousel({
+  interval: 15000
+})
+
+// Nav scroll
+$('#myScrollspy').scrollspy({
+
+});
