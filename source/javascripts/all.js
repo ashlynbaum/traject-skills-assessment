ad1b237083e5ec5b3fb8a722248d4499c62ead8d
Ashlyn Baum <a@ashlynbaum.com>
2015-05-13 18:19:50 +1200

    Add functionality to navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,12 +2,33 @@
 //= require jquery
 //= require bootstrap-sprockets
 
-// carousel
-$('.carousel').carousel({
-  interval: 15000
-})
 
-// Nav scroll
-$('#myScrollspy').scrollspy({
+$( document ).ready(function() {
+  // carousel
+  $('.carousel').carousel({
+    interval: 15000
+  })
+
+  $('body').scrollspy({ target: '#myScrollspy', offset: 50 })
+
+  // affix nav bar
+  $('#myScrollspy').affix({
+    offset: { top: 250 }
+  })
+
+  $('#myScrollspy').on('affix-top.bs.affix', function(){
+    $('#myScrollspy').addClass('transparent');
+    $('#myScrollspy').removeClass('navbar-inverse');
+    $('#myScrollspy').removeClass('navbar-default');
+    $('#myScrollspy collapse').removeClass('navbar-inverse');
+    $('#myScrollspy navbar-right').removeClass('navbar-inverse');
+  });
+  $('#myScrollspy').on('affix.bs.affix', function(){
+      $('#myScrollspy').removeClass('transparent');
+      $('#myScrollspy').addClass('navbar-inverse');
+      $('#myScrollspy').addClass('navbar-default');
+      $('#myScrollspy collapse').addClass('navbar-inverse');
+      $('#myScrollspy navbar-right').addClass('navbar-inverse');
+  });
 
 });
